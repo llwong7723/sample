da09eba63942b070b4450f7bff3711378d785f78
Frederick <fredericktham.tham@gmail.com>
2020-01-09 01:05:45 +0800

    Minor Update

--- catalog/view/javascript/custom/pushy.js
+++ catalog/view/javascript/custom/pushy.js
@@ -1,8 +1,3 @@
-/*! Pushy - v1.3.0 - 2019-6-25
-* Pushy is a responsive off-canvas navigation menu using CSS transforms & transitions.
-* https://github.com/christophery/pushy/
-* by Christopher Yee */
-
 (function ($) {
 	var pushy = $('.pushy'), //menu css class
 		body = $('body'),
